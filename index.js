52dff985460bd9469219f2bb22720273844a0f7b
isaiful508 <isaiful.islam508@gmail.com>
2024-05-10 23:58:54 +0600

    added room details api in server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require ('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.port || 5000
 
 const app = express()
@@ -53,7 +53,15 @@ app.get('/rooms', async(req, res) =>{
     res.send(result);
 })
 
+//rooms by id
 
+app.get('/room-details/:id', async (req, res) =>{
+    const id = req.params.id
+    const query = {_id : new ObjectId(id)}
+    const result = await roomsCollection.findOne(query);
+    res.send(result);
+  })
+  
 
 
 
